f1bbec473c7221bc4d08fda559e1710de7720246
simpart <simpart>
2018-12-30 03:34:38 +0000

    mod addItem

--- index.js
+++ index.js
@@ -25,50 +25,73 @@ mf.comp.AcdMenu = class extends Menu {
         }
     }
     
-    addItem(prm) {
+    /**
+     * get only menu item
+     * this method is custmaized from menu
+     */
+    item (prm) {
         try {
-            this.addChild(prm);
-            let clk_evt = (clk_cmp, clk_evt, clk_prm) => {
+            let ret = super.item(prm);
+            if (undefined !== ret) {
+                let itm_ret = [];
+                let acc_chd = null;
+                for (let ridx in ret) {
+                    if (true === mf.func.isComp(ret[ridx], 'Accordion')) {
+                        acc_chd = ret[ridx].child();
+                        for (let aidx in acc_chd) {
+                            itm_ret.push(acc_chd[aidx]);
+                        }
+                    } else {
+                        itm_ret.push(ret[ridx]);
+                    }
+                }
+                return itm_ret;
+            }
+            
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * add menu item clickevent
+     *
+     * @note private method
+     */
+    addItem (prm) {
+        try {
+            let ret = super.addItem(prm);
+            if (undefined === prm) {
+                /* getter */
+                return ret;
+            }
+            /* setter */
+            if (true !== mf.func.isComp(prm, 'Accordion')) {
+                return;
+            }
+            
+            let fnc = (clk_cmp, clk_evt, clk_prm) => {
                 try {
                     let itm = clk_prm.item();
-                    let cnt = 0;
                     for (let iidx in itm) {
-                        if (true === mf.func.isInclude(itm[iidx], ['Component', 'Accordion'])) {
-                            let acd_chd = itm[iidx].child();
-                            for (let aidx in acd_chd) {
-                                if (clk_cmp.getId() === acd_chd[aidx].getId()) {
-                                    clk_prm.execSelect(cnt);
-                                    return;
-                                }
-                                cnt++;
-                            }
-                        } else {
-                            if (clk_cmp.getId() === itm[iidx].getId()) {
-                                clk_prm.execSelect(cnt);
-                                return;
-                            }
-                            cnt++;
+                        if (clk_cmp.getId() === itm[iidx].getId()) {
+                            clk_prm.select(parseInt(iidx));
                         }
                     }
                 } catch (e) {
                     console.error(e.stack);
                     throw e;
                 }
-            }
-            
-            if (true === mf.func.isInclude(prm, ['Component', 'Accordion'])) {
-                let accd_chd = prm.child();
-                for (let aidx in accd_chd) {
-                    accd_chd[aidx].execOption({
-                        event : [new Click([clk_evt, this])]
+            };
+            let acc_chd = prm.child();
+            for (let aidx in acc_chd) {
+                if (true === mf.func.isComp(acc_chd[aidx], 'MenuItem')) {
+                    acc_chd[aidx].execOption({
+                        event : [new Click([fnc, this])],
                     });
                 }
-            } else {
-                prm.execOption({
-                    event : [new Click([clk_evt, this])]
-                });
             }
-            this.arrayMember('item', 'Component', prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
